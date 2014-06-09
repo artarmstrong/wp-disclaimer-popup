69939e12d619f3d0e4a21a4855443c22f8130027
Art Armstrong <me@artarmstrong.com>
2014-06-09 10:49:44 -0700

    Update the opacity of the background

--- _js/wp-disclaimer-popup.js
+++ _js/wp-disclaimer-popup.js
@@ -4,12 +4,12 @@ jQuery(document).ready(function($) {
 	var cookie_expire = parseInt( $('#wpd-vars').children('#wpd-expire').text() );
 	var post_id = parseInt( $('#wpd-vars').children('#wpd-id').text() );
 	if(!cookie_expire){ cookie_expire = 1; }
-	
+
   // Check for cookie, otherwise show
   if($.cookie('wpd-'+post_id) == 'agreed'){
     // do nothing
   }else{
-  
+
     // Call fancybox
   	$("#wpd-disclaimer").fancybox({
   		maxWidth	: 800,
@@ -22,11 +22,11 @@ jQuery(document).ready(function($) {
   		closeEffect	: 'none',
   		closeBtn : false,
   		closeClick  : false,
-  		helpers   : { 
+  		helpers   : {
         overlay : {
-          closeClick: false, // prevents closing when clicking OUTSIDE fancybox 
+          closeClick: false, // prevents closing when clicking OUTSIDE fancybox
           css : {
-            'background' : 'rgba(0, 0, 0, 0.9)'
+            'background' : 'rgba(0, 0, 0, 0.8)'
           }
         }
       },
@@ -34,22 +34,22 @@ jQuery(document).ready(function($) {
         close  : null
       },
       overlay: {
-        opacity: 0.8, // or the opacity you want 
+        opacity: 0.8, // or the opacity you want
       }
   	}).trigger('click');
-  	
+
   	// Agree button
   	$('#wpd-disclaimer .agree').click(function(){
   		$.cookie('wpd-'+post_id, 'agreed', { expires: cookie_expire });
   		$.fancybox.close();
-  		return false; 
+  		return false;
   	});
-  	
+
   	// Disagree button
   	$('#wpd-disclaimer .disagree').click(function(){
   		window.location.href = $(this).attr('href')
   	});
-	
+
 	}
-	
+
 });
